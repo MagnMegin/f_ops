c0e17367c450f490df518afefb2fb44255efcb54
MagnMegin <magne.myran@skomakerhuset.no>
2024-11-20 01:52:43 +1100

    Cleaned up parser mod

--- src/parser.rs
+++ src/parser.rs
@@ -2,7 +2,6 @@ use std::fmt::Display;
 use crate::tokens::Token;
 
 
-
 #[derive(Debug)]
 pub struct ParserError(String);
 
@@ -12,55 +11,6 @@ impl Display for ParserError {
     }
 }
 
-fn brackets_validated(tokens: &mut Vec<Token>) -> bool {
-    
-    todo!("Implement later")
-}
-
-fn simplify_subtraction(tokens: &mut Vec<Token>) {
-    let mut prev_token = Token::Add; //Add is an operation so if the first token is Sub it will be converted to Neg
-
-    // for i in 0..tokens.len() {
-    //     if tokens[i] == Token::Sub {
-    //         if prev_token == Token
-    //     }
-
-    //     prev_token = tokens[i].clone();
-
-    // }
-}
-
-fn parse(tokens: &mut Vec<Token>) -> bool{
-    if !brackets_validated(tokens) {
-        return false;
-    }
-    return true;
-}
-
-struct Function {
-    
-}
-
-struct Variable {
-
-}
-
-enum Expression {
-    Const(f32),
-    Var(String),
-    Add(Box<Expression>, Box<Expression>),
-    Mul(Box<Expression>, Box<Expression>),
-    Func(String, Box<Expression>),
-}
-
-impl Expression {
-    fn evaluate(&self) -> f32 {
-        match self {
-            Self::Const(x) => *x,
-            Self::Var(s) => s.len() as f32,
-            Self::Add(e1, e2) => e1.evaluate() + e2.evaluate(),
-            Self::Mul(e1, e2) => e1.evaluate() + e2.evaluate(),
-            Self::Func(_s, e) => e.evaluate(),
-        }
-    }
+fn to_postfix(tokens: Vec<Token>) -> Vec<Token> {
+    todo!()
 }
