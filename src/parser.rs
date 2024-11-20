d6a03c3460247c7ad6ba96b5f1264367b2d880e7
MagnMegin <magne.myran@skomakerhuset.no>
2024-11-20 23:59:04 +1100

    Wrote name to func/var parser and a test

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, process::Output};
 use crate::tokens::Token;
 
 
@@ -27,9 +27,50 @@ fn verify_brackets(tokens: &Vec<Token>) -> bool {
 }
 
 fn parse_names(tokens: &mut Vec<Token>) {
+    let mut index = 0;
     
+    while let Some(t) = tokens.get(index) {
+        if let Token::Name(name) = t {
+            if tokens.get(index + 1) == Some(&Token::LBracket) {
+                tokens[index] = Token::Func(name.to_string());
+            }
+            else {
+                tokens[index] = Token::Var(name.to_string());
+            }
+        }
+
+        index += 1;
+    }
 }
 
 fn to_postfix(tokens: Vec<Token>) -> Vec<Token> {
     todo!()
+}
+
+#[test]
+fn test_parse_names() {
+    let mut input = vec![
+        Token::Name("exp".to_string()),
+        Token::LBracket,
+        Token::Name("x".to_string()),
+        Token::Add,
+        Token::Constant(1.0),
+        Token::RBracket,
+        Token::Mul,
+        Token::Name("y".to_string())
+        ];
+    
+    let output = vec![
+        Token::Func("exp".to_string()),
+        Token::LBracket,
+        Token::Var("x".to_string()),
+        Token::Add,
+        Token::Constant(1.0),
+        Token::RBracket,
+        Token::Mul,
+        Token::Var("y".to_string())
+        ];
+    parse_names(&mut input);
+
+    assert!(input == output)
 }
